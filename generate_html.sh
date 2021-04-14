b10a371d6ae2414dcae6976704e7c018c9c26efe
sherleywang <sherleywang@ymail.com>
2021-04-14 13:40:29 -0500

    removed bagging and boosting, added models for raw features

--- generate_html.sh
+++ generate_html.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 jupyter nbconvert --to html data_overview.ipynb
 jupyter nbconvert --to html models/training_models.ipynb
+jupyter nbconvert --to html models/large_models.ipynb
+jupyter nbconvert --to html models/neural_network_colab.ipynb
 jupyter nbconvert --to html music/feature_extraction.ipynb
